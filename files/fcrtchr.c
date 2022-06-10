77b6c54b1f35260f627b9955d9aab1e6676aad8b
vacvvn <vacvvn2012@gmail.com>
2022-06-10 14:40:20 +0300

    перенес ф-ции таймера в главный файл

--- files/fcrtchr.c
+++ files/fcrtchr.c
@@ -92,6 +92,49 @@ struct fcrt_priv_mem fcrt_mem;
 void * fcrt_alloc(uint32_t align, unsigned int sz_b, dma_addr_t * dma_addr);
 void fcrt_free(void * addr);
 
+//////////////
+#define	HRTIMER0_US	(100 * 1000 * 1000)
+ 
+static struct hrtimer g_hrtimer0;
+static u64 nsec_time = HRTIMER0_US;
+ 
+static enum hrtimer_restart hrtimer_test_fn(struct hrtimer *hrtimer)
+{
+	static u64 divider =0;
+	divider++;
+	if(divider == 10)
+	{
+		pr_err("#### hrtimer timeout: 1Sec");
+		divider = 0;
+	}
+	
+    hrtimer_forward_now(hrtimer, ns_to_ktime(nsec_time));
+	return HRTIMER_RESTART;
+}
+ 
+int rtsk_init_task(u64 nsec_period)
+{
+	pr_err("#### hr_timer_test module init...\n");
+	
+	nsec_time = nsec_period;
+	struct hrtimer *hrtimer = &g_hrtimer0;
+	hrtimer_init(hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
+	hrtimer->function = hrtimer_test_fn;
+	hrtimer_start(hrtimer, ns_to_ktime(nsec_period),
+		      HRTIMER_MODE_REL_PINNED);
+    
+	return 0;
+}
+ 
+void hr_timer_test_exit (void)
+{
+	struct hrtimer *hrtimer = &g_hrtimer0;
+	hrtimer_cancel(hrtimer);
+    pr_err("#### hr_timer_test module exit...\n");
+}
+
+/////////////////
+
 static void fcrt_txdesc_out(struct device * dev, FCRT_TX_DESC * dp)
 {
     dev_info(dev,
@@ -437,7 +480,7 @@ static int fcrtchr_probe(struct platform_device *pdev)
 	printk(KERN_INFO "fcrtch: major = %d minor = %d\n", fcrtchr_major, fcrtchr_minor);
 
 	dev_set_drvdata(dev, lp);
-	// rtsk_init_task(100 * 1000 * 1000);
+	rtsk_init_task(100 * 1000 * 1000);
     return 0;
 ///////////////////////////
 error6:
